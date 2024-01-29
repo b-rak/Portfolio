58cf6d34d401b7dbb41d95e03ab32f129919a7b3
b-rak <b.tas93@googlemail.com>
2024-01-29 21:48:04 +0100

    icon components

---
+++ components/FAIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-6 h-6 flex items-center justify-center shrink-0">
+    <font-awesome-icon
+      :icon="{
+        prefix: iconPrefix,
+        iconName: iconName,
+      }"
+      :class="iconWidth"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  iconPrefix: string;
+  iconName: string;
+  iconWidth: string;
+}>();
+</script>
---
+++ components/LinkIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <a :href="to" target="_blank" rel="noreferrer" class="cursor-pointer">
+    <font-awesome-icon
+      :icon="{ prefix: iconPrefix, iconName: iconName }"
+      :class="`${iconWidth} text-white hover:text-lightGray`"
+    />
+  </a>
+</template>
+
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    iconName: string;
+    iconPrefix: string;
+    to: string;
+    iconWidth?: string;
+  }>(),
+  {
+    iconWidth: "w-6",
+  }
+);
+</script>
