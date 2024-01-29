5106570f691964803c498d285f6e6f590c78b0d1
b-rak <b.tas93@googlemail.com>
2024-01-29 21:47:49 +0100

    landing page

---
+++ components/LandingHeading.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 class="font-handwriting text-5xl text-lightGray">
+    Hey
+    <span id="itsme" class="text-brightRed inline-block">it's-a me</span>
+    <span class="block mt-2 md:inline-block md:pl-4">Burak!</span>
+  </h1>
+</template>
+
+<style scoped>
+#itsme {
+  animation: myAnim 0.75s ease-in-out 0s 1 normal forwards;
+}
+
+@keyframes myAnim {
+  0% {
+    transform: scale(0.1);
+  }
+
+  50% {
+    transform: scale(1.2);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="mt-[5%] md:ml-[10%]">
+    <template v-if="viewportXS">
+      <div class="w-full h-56 relative">
+        <LandingHeading />
+        <img
+          src="/images/rocketOnly.png"
+          alt="rocket-with-profile-picture"
+          class="w-48 h-48 absolute right-0 bottom-0"
+        />
+      </div>
+      <div class="flex flex-col items-center gap-4 mb-4">
+        <div
+          class="text-lightGray sm:w-[30rem] lg:w-[40rem] rounded-lg shadow_light_4 bg-darkCharcoal border-solid border-oliveGreen p-4 text-justify text_regular_18"
+        >
+          <p>{{ $t("landing") }}</p>
+        </div>
+        <button
+          @click="navigateTo(localePath('/aboutme'))"
+          class="text-warmOrange border-2 border-warmOrange rounded-md px-2 py-1 w-fit"
+        >
+          {{ $t("aboutme.title") }}
+        </button>
+      </div>
+    </template>
+    <template v-else>
+      <LandingHeading />
+      <div
+        class="text-lightGray sm:w-[30rem] lg:w-[40rem] mt-16 rounded-lg shadow_light_4 bg-darkCharcoal border-solid border-oliveGreen p-4 text-justify text_regular_18"
+      >
+        <p>{{ $t("landing") }}</p>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  title: "Landing Page",
+});
+
+const localePath = useLocalePath();
+
+const breakpointsTailwind = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  "2xl": 1536,
+};
+const viewportXS = ref(useBreakpoints(breakpointsTailwind).smaller("sm"));
+</script>
