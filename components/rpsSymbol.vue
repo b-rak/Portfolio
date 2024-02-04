def98eddaefddad455abac2b18812caf9b1004dc
b-rak <b.tas93@googlemail.com>
2024-02-04 16:26:55 +0100

    update rpsSymbol

--- components/rpsSymbol.vue
+++ components/rpsSymbol.vue
@@ -2,14 +2,15 @@
   <div
     class="h-[20rem] w-[18rem] xl:h-[22rem] xl:w-[20rem] shadow-[0px_0px_0px_2px_#000000] p-8 flex flex-col gap-2 justify-center items-center"
     :class="{
-      'cursor-pointer hover:shadow-[0px_0px_0px_8px_#ffb800]': !result,
+      'cursor-pointer hover:shadow-[0px_0px_0px_8px_#855000] dark:hover:shadow-[0px_0px_0px_8px_#ff9800]':
+        !result,
     }"
     @click="$emit('selected', symbol)"
   >
     <slot />
     <button
       :id="symbol"
-      class="flex flex-col gap-2 text_regular_18 text-darkGray dark:text-lightGray"
+      class="flex flex-col gap-2 text_regular_18 text-darkGray dark:text-lightGray items-center"
       :class="{ 'cursor-default': result }"
     >
       <img
